a08a3aafea3905eee07a0eea9ad0563ec59ba303
iTecAI <matteovh@gmail.com>
2020-08-02 06:54:01 -0400

    decluttering

--- setup.py
+++
@@ -1,8 +0,0 @@
-from subprocess import run
-
-with open('requirements.txt','r') as f:
-    libs = f.read().split('\n')
-    for lib in libs:
-        print('Installing/Upgrading '+lib)
-        run(['pip','install','--upgrade',lib])
-        print('Complete')
